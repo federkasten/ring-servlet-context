743697574b119c213e300fac84e3bab022f22fda
alumi <alumi@users.noreply.github.com>
2018-06-04 15:38:32 +0900

    Use clojure 1.9

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://opensource.org/licenses/MIT"}
   ;; :aliases {"test-all" ["with-profile" "default:+1.6:+1.7:+1.8" "test"]}
   :profiles
-  {:dev {:dependencies [[org.clojure/clojure "1.5.1"]]}
+  {:dev {:dependencies [[org.clojure/clojure "1.9.0"]]}
    :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
    :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
    :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}})
