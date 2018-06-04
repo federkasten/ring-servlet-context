0f500ff84a6e0891b7c3deac1c3baac3646767af
alumi <alumi@users.noreply.github.com>
2018-06-04 15:39:09 +0900

    Support async ring handlers

--- src/ring/middleware/servlet_context.clj
+++ src/ring/middleware/servlet_context.clj
@@ -1,15 +1,19 @@
-(ns ring.middleware.servlet-context)
+(ns ring.middleware.servlet-context
+  (:import javax.servlet.http.HttpServletRequest))
+
+(defn- update-uri [{:keys [servlet-request ^String uri] :as request}]
+  (if servlet-request
+    (let [context (.getContextPath ^HttpServletRequest servlet-request)]
+      (if (.startsWith uri context)
+        (assoc request :uri (.substring uri (.length context)))
+        request))
+    request))
 
 (defn wrap-servlet-context
   "Removes the servlet context path from requested URI"
   [handler]
-  (fn [request]
-    (handler
-     (if-let [servlet-req (:servlet-request request)]
-       (let [context (.getContextPath ^javax.servlet.http.HttpServletRequest servlet-req)
-             uri ^String (:uri request)]
-         (if (.startsWith uri context)
-           (assoc request :uri
-                  (.substring uri (.length context)))
-           request))
-       request))))
+  (fn servlet-context-wrapped-handler
+    ([request]
+     (handler (update-uri request)))
+    ([request respond raise]
+     (handler (update-uri request) respond raise))))
